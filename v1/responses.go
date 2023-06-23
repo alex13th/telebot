2523dabce1c6908f60acfda39ee4dac06fe56595
alex13th <alex13th76@gmail.com>
2023-06-23 16:50:50 +0300

    Refactored response tests

--- v1/responses.go
+++ v1/responses.go
@@ -2,6 +2,7 @@ package telebot
 
 import (
 	"encoding/json"
+	"fmt"
 	"io"
 )
 
@@ -36,28 +37,64 @@ func (ur *UpdateResponse) Parse(reader io.Reader) error {
 	}
 
 	for i, update := range ur.Result {
-		ur.Result[i].Message = normalizeMessage(update.Message)
-		ur.Result[i].EditedMessage = normalizeMessage(update.EditedMessage)
-		ur.Result[i].ChannelPost = normalizeMessage(update.ChannelPost)
-		ur.Result[i].EditedChannelPost = normalizeMessage(update.EditedChannelPost)
+		var err error
+		if ur.Result[i].Message, err = normalizeMessage(update.Message); err != nil {
+			*ur = UpdateResponse{}
+			return err
+		}
+		if ur.Result[i].EditedMessage, err = normalizeMessage(update.EditedMessage); err != nil {
+			*ur = UpdateResponse{}
+			return err
+		}
+		if ur.Result[i].ChannelPost, err = normalizeMessage(update.ChannelPost); err != nil {
+			*ur = UpdateResponse{}
+			return err
+		}
+		if ur.Result[i].EditedChannelPost, err = normalizeMessage(update.EditedChannelPost); err != nil {
+			*ur = UpdateResponse{}
+			return err
+		}
 	}
 	return nil
 }
 
-func normalizeMessage(m Message) Message {
+func normalizeMessage(m Message) (Message, error) {
+	if m.Chat.Id == nil {
+		return m, nil
+	}
+
 	switch val := m.Chat.Id.(type) {
 	case float64:
 		m.Chat.Id = int(val)
+	case int:
+	case string:
+	default:
+		return Message{}, fmt.Errorf("invalid chat Id type %v", val)
 	}
+
 	if m.ReplyToMessage != nil {
 		switch val := m.ReplyToMessage.Chat.Id.(type) {
 		case float64:
 			m.ReplyToMessage.Chat.Id = int(val)
+		case int:
+		case string:
+		default:
+			return Message{}, fmt.Errorf("invalid ReplyToMessage chat Id type %v", val)
 		}
 	}
-	return m
+	return m, nil
 }
 
-func (message *MessageResponse) Parse(reader io.Reader) error {
-	return ParseJson(message, reader)
+func (mr *MessageResponse) Parse(reader io.Reader) error {
+	err := ParseJson(mr, reader)
+	if err != nil {
+		return err
+	}
+	mr.Result, err = normalizeMessage(mr.Result)
+
+	if err != nil {
+		*mr = MessageResponse{}
+		return err
+	}
+	return nil
 }
--- v1/responses_test.go
+++ v1/responses_test.go
@@ -1,97 +1,286 @@
 package telebot
 
 import (
-	"fmt"
 	"strings"
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
 )
 
-func TestParseResponse(t *testing.T) {
-	updateCount := 3
-	json := `{
-		"ok": true,
-		"result": [
-			{
-				"update_id": 123130161,
-				"message": {
+func Test_normalizeMessage(t *testing.T) {
+	tests := []struct {
+		name    string
+		m       Message
+		want    Message
+		wantErr bool
+	}{
+		{name: "Chat is int", m: Message{Chat: Chat{Id: 10}}, want: Message{Chat: Chat{Id: 10}}},
+		{name: "Chat is string", m: Message{Chat: Chat{Id: "@username"}}, want: Message{Chat: Chat{Id: "@username"}}},
+		{name: "Chat is int float64", m: Message{Chat: Chat{Id: float64(123456789123456)}}, want: Message{Chat: Chat{Id: 123456789123456}}},
+		{name: "Chat is fract float64", m: Message{Chat: Chat{Id: float64(1234567891.23456)}}, want: Message{Chat: Chat{Id: 1234567891}}},
+		{name: "Chat is bool", m: Message{Chat: Chat{Id: true}}, wantErr: true},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := normalizeMessage(tt.m)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("normalizeMessage() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if diff := cmp.Diff(got, tt.want); diff != "" {
+				t.Errorf("normalizeMessage() difference: %s", diff)
+			}
+		})
+		t.Run("ReplyToMessage - "+tt.name, func(t *testing.T) {
+			m := Message{Chat: Chat{Id: 10}, ReplyToMessage: &tt.m}
+			want := Message{}
+			if !tt.wantErr {
+				want = Message{Chat: Chat{Id: 10}, ReplyToMessage: &tt.want}
+			}
+			got, err := normalizeMessage(m)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("normalizeMessage() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if diff := cmp.Diff(got, want); diff != "" {
+				t.Errorf("normalizeMessage() difference: %s", diff)
+			}
+		})
+	}
+}
+
+func TestMessageResponse_Parse(t *testing.T) {
+	tests := []struct {
+		name    string
+		json    string
+		want    MessageResponse
+		wantErr bool
+	}{
+		{
+			name: "Chat id is int",
+			json: `{
+				"ok": true,
+				"result": {
 					"message_id": 2468,
-					"from": {"id": 1,"is_bot": false,"first_name": "Alexey","last_name": "Sukharev","language_code": "en"},
+					"from": {"id": 10,"is_bot": false,"first_name": "Alexey","last_name": "Sukharev","language_code": "en"},
 					"chat": {"id": 1,"first_name": "Alexey","last_name": "Sukharev","type": "private"},
 					"date": 1630134810,
 					"text": "Hello world!!!"
 				}
+			}`,
+			want: MessageResponse{
+				Ok: true,
+				Result: Message{
+					MessageId: 2468,
+					From:      User{Id: 10, IsBot: false, FirstName: "Alexey", LastName: "Sukharev", LanguageCode: "en"},
+					Chat:      Chat{Id: 1, FirstName: "Alexey", LastName: "Sukharev", Type: "private"},
+					Date:      1630134810,
+					Text:      "Hello world!!!",
+				},
 			},
-			{
-				"update_id": 123130162,
-				"message": {
-					"message_id": 2469,
-					"from": {"id": 2,"is_bot": false,"first_name": "Alexey","last_name": "Sukharev","language_code": "en"},
-					"chat": {"id": 100,"first_name": "Alexey","last_name": "Sukharev","type": "private"},
-					"date": 1630135377,
-					"text": "\u041f\u0440\u0438\u0432\u0435\u0442!"
+		},
+		{
+			name: "Chat id is string",
+			json: `{
+				"ok": true,
+				"result": {
+					"message_id": 2468,
+					"from": {"id": 10,"is_bot": false,"first_name": "Alexey","last_name": "Sukharev","language_code": "en"},
+					"chat": {"id": "@username","first_name": "Alexey","last_name": "Sukharev","type": "private"},
+					"date": 1630134810,
+					"text": "Hello world!!!"
 				}
+			}`,
+			want: MessageResponse{
+				Ok: true,
+				Result: Message{
+					MessageId: 2468,
+					From:      User{Id: 10, IsBot: false, FirstName: "Alexey", LastName: "Sukharev", LanguageCode: "en"},
+					Chat:      Chat{Id: "@username", FirstName: "Alexey", LastName: "Sukharev", Type: "private"},
+					Date:      1630134810,
+					Text:      "Hello world!!!",
+				},
 			},
-			{
-				"update_id": 123130163,
-				"message": {
-					"message_id": 2470,
-					"from": {"id": 3,"is_bot": false,"first_name": "Alexey","last_name": "Sukharev","language_code": "en"},
-					"chat": {"id": 100,"first_name": "Alexey","last_name": "Sukharev","type": "private"},
-					"date": 1630135517,
-					"text": "Hello!"
+		},
+		{
+			name: "Wrong chat id",
+			json: `{
+				"ok": true,
+				"result": {
+					"message_id": 2468,
+					"from": {"id": 10,"is_bot": false,"first_name": "Alexey","last_name": "Sukharev","language_code": "en"},
+					"chat": {"id": true,"first_name": "Alexey","last_name": "Sukharev","type": "private"},
+					"date": 1630134810,
+					"text": "Hello world!!!"
 				}
-			}
-		]
-	}`
+			}`,
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			mr := &MessageResponse{}
+			err := mr.Parse(strings.NewReader(tt.json))
 
-	expUpdate := Update{
-		UpdateId: 123130161,
-		Message: Message{
-			MessageId: 2468,
-			From: User{
-				Id:           1,
-				IsBot:        false,
-				FirstName:    "Alexey",
-				LastName:     "Sukharev",
-				LanguageCode: "en",
+			if (err != nil) != tt.wantErr {
+				t.Errorf("MessageResponse.Parse() error = %v, wantErr %v", err, tt.wantErr)
+			}
+			if diff := cmp.Diff(*mr, tt.want); diff != "" {
+				t.Errorf("MessageResponse.Parse() difference: %s", diff)
+			}
+		})
+	}
+}
+
+func TestUpdateResponse_Parse(t *testing.T) {
+	tests := []struct {
+		name    string
+		json    string
+		want    UpdateResponse
+		wantErr bool
+	}{
+		{
+			name: "Two updates",
+			json: `{
+				"ok": true,
+				"result": [
+					{
+						"update_id": 123130160,
+						"message": {
+							"message_id": 2468,
+							"from": {"id": 10,"is_bot": false,"first_name": "Alexey","last_name": "Sukharev","language_code": "en"},
+							"chat": {"id": 1,"first_name": "Alexey","last_name": "Sukharev","type": "private"},
+							"date": 1630134810,
+							"text": "Hello world!!!"
+						}
+					},
+					{
+						"update_id": 123130161,
+						"message": {
+							"message_id": 2469,
+							"from": {"id": 11,"is_bot": false,"first_name": "Alexey","last_name": "Sukharev","language_code": "en"},
+							"chat": {"id": 1,"first_name": "Alexey","last_name": "Sukharev","type": "private"},
+							"date": 1630134810,
+							"text": "Hello world!!!"
+						}
+					}
+				]
+			}`,
+			want: UpdateResponse{
+				Ok: true,
+				Result: []Update{
+					{
+						UpdateId: 123130160,
+						Message: Message{
+							MessageId: 2468,
+							From:      User{Id: 10, IsBot: false, FirstName: "Alexey", LastName: "Sukharev", LanguageCode: "en"},
+							Chat:      Chat{Id: 1, FirstName: "Alexey", LastName: "Sukharev", Type: "private"},
+							Date:      1630134810,
+							Text:      "Hello world!!!",
+						},
+					},
+					{
+						UpdateId: 123130161,
+						Message: Message{
+							MessageId: 2469,
+							From:      User{Id: 11, IsBot: false, FirstName: "Alexey", LastName: "Sukharev", LanguageCode: "en"},
+							Chat:      Chat{Id: 1, FirstName: "Alexey", LastName: "Sukharev", Type: "private"},
+							Date:      1630134810,
+							Text:      "Hello world!!!",
+						},
+					},
+				},
 			},
-			Chat: Chat{
-				Id:        1,
-				FirstName: "Alexey",
-				LastName:  "Sukharev",
-				Type:      "private",
-			},
-			Date: 1630134810,
-			Text: "Hello world!!!",
+		},
+		{
+			name: "Wrong message chat id",
+			json: `{
+				"ok": true,
+				"result": [
+					{
+						"update_id": 123130160,
+						"message": {
+							"message_id": 2468,
+							"from": {"id": 10,"is_bot": false,"first_name": "Alexey","last_name": "Sukharev","language_code": "en"},
+							"chat": {"id": true,"first_name": "Alexey","last_name": "Sukharev","type": "private"},
+							"date": 1630134810,
+							"text": "Hello world!!!"
+						}
+					}
+				]
+			}`,
+			wantErr: true,
+		},
+		{
+			name: "Wrong EditedMessage chat id",
+			json: `{
+				"ok": true,
+				"result": [
+					{
+						"update_id": 123130160,
+						"edited_message": {
+							"message_id": 2468,
+							"from": {"id": 10,"is_bot": false,"first_name": "Alexey","last_name": "Sukharev","language_code": "en"},
+							"chat": {"id": true,"first_name": "Alexey","last_name": "Sukharev","type": "private"},
+							"date": 1630134810,
+							"text": "Hello world!!!"
+						}
+					}
+				]
+			}`,
+			wantErr: true,
+		},
+		{
+			name: "Wrong ChannelPost chat id",
+			json: `{
+				"ok": true,
+				"result": [
+					{
+						"update_id": 123130160,
+						"channel_post": {
+							"message_id": 2468,
+							"from": {"id": 10,"is_bot": false,"first_name": "Alexey","last_name": "Sukharev","language_code": "en"},
+							"chat": {"id": true,"first_name": "Alexey","last_name": "Sukharev","type": "private"},
+							"date": 1630134810,
+							"text": "Hello world!!!"
+						}
+					}
+				]
+			}`,
+			wantErr: true,
+		},
+		{
+			name: "Wrong EditedChannelPost chat id",
+			json: `{
+				"ok": true,
+				"result": [
+					{
+						"update_id": 123130160,
+						"edited_channel_post": {
+							"message_id": 2468,
+							"from": {"id": 10,"is_bot": false,"first_name": "Alexey","last_name": "Sukharev","language_code": "en"},
+							"chat": {"id": true,"first_name": "Alexey","last_name": "Sukharev","type": "private"},
+							"date": 1630134810,
+							"text": "Hello world!!!"
+						}
+					}
+				]
+			}`,
+			wantErr: true,
 		},
 	}
 
-	resp := UpdateResponse{}
-	err := resp.Parse(strings.NewReader(json))
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ur := &UpdateResponse{}
+			err := ur.Parse(strings.NewReader(tt.json))
 
-	t.Run("Error is nil", func(t *testing.T) {
-		if err != nil {
-			t.Fail()
-		}
-	})
-
-	t.Run("Response Ok", func(t *testing.T) {
-		if !resp.Ok {
-			t.Fail()
-		}
-	})
-
-	t.Run(fmt.Sprintf("Update count is %d", updateCount), func(t *testing.T) {
-		if len(resp.Result) != 3 {
-			t.Fail()
-		}
-	})
-
-	t.Run("First update properties", func(t *testing.T) {
-		if diff := cmp.Diff(expUpdate, resp.Result[0]); diff != "" {
-			t.Errorf("First update difference: = %v", diff)
-		}
-	})
+			if (err != nil) != tt.wantErr {
+				t.Errorf("UpdateResponse.Parse() error = %v, wantErr %v", err, tt.wantErr)
+			}
+			if diff := cmp.Diff(*ur, tt.want); diff != "" {
+				t.Errorf("UpdateResponse.Parse() difference: %s", diff)
+			}
+		})
+	}
 }
